e8a4b7b0ccb4183ae10f46be55deea5c6c1a0870
Zongjian Yang <122143424+Saury997@users.noreply.github.com>
2024-12-05 11:01:42 +0800

    update model and __init__.py for exp3

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@
 * Author: Zongjian Yang
 * Date: 2024/11/19 下午2:51 
 * Project: AI_Intro 
-* File: __init__.py.py
+* File: __init__.py
 * IDE: PyCharm 
 * Function:
 """
@@ -13,3 +13,4 @@ from .resnet import ResNet
 from .vgg import VGG
 from .mlp import MLP
 from .cnn import CNN
+from .unet import UNet
